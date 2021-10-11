094968b462d5b8248da3aa8f5306961f62014934
João Pedro Bandeira D'Avila <joao.davila@itsgroup.com.br>
2021-10-10 21:47:24 -0300

    Adding learn more button

--- app/src/main/java/com/jp/aboutme/MainActivity.kt
+++ app/src/main/java/com/jp/aboutme/MainActivity.kt
@@ -2,10 +2,18 @@ package com.jp.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setWidgets()
+    }
+
+    private fun setWidgets(){
+        findViewById<Button>(R.id.bt_learn_more)?.setOnClickListener {
+            //
+        }
     }
 }
