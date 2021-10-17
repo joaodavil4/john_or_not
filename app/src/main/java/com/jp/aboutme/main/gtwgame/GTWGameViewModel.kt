801e386eb3602552666dc424a7836083d541f484
João Pedro Bandeira D'Avila <joao.davila@itsgroup.com.br>
2021-10-17 14:35:47 -0300

    Transformation variable

--- app/src/main/java/com/jp/aboutme/main/gtwgame/GTWGameViewModel.kt
+++ app/src/main/java/com/jp/aboutme/main/gtwgame/GTWGameViewModel.kt
@@ -1,7 +1,11 @@
 package com.jp.aboutme.main.gtwgame
 
+import android.os.CountDownTimer
+import android.text.format.DateUtils
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import timber.log.Timber
 
@@ -22,12 +26,50 @@ class GTWGameViewModel : ViewModel() {
     val eventGameFinish: LiveData<Boolean>
         get() = _eventGameFinish
 
+    // Countdown time
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+
+    private val timer: CountDownTimer
+
+    // The String version of the current time
+    val currentTimeString = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time)
+    }
+
+
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
     init {
         _word.value = ""
         _score.value = 0
+        resetList()
+        nextWord()
+
+        // Creates a timer which triggers the end of the game when it finishes
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+
+            override fun onTick(millisUntilFinished: Long)
+            {
+                _currentTime.value = millisUntilFinished/ONE_SECOND
+            }
+
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                onGameFinish()
+            }
+
+
+        }
+
+        timer.start()
+
+
+
+
     }
 
     /**
@@ -60,18 +102,13 @@ class GTWGameViewModel : ViewModel() {
         wordList.shuffle()
     }
 
-    init {
-        Timber.i("GTWGameViewModel created!")
-        resetList()
-        nextWord()
-    }
-
     /**
      * Callback called when the ViewModel is destroyed
      */
     override fun onCleared() {
         super.onCleared()
-        Timber.i("GTWGameViewModel destroyed!")
+        Timber.i("GameViewModel destroyed!")
+        timer.cancel()
     }
 
     /** Methods for updating the UI **/
@@ -89,8 +126,8 @@ class GTWGameViewModel : ViewModel() {
      */
     private fun nextWord() {
         //Select and remove a word from the list
-        if (wordList.isNotEmpty()) {
-            _word.value = wordList.removeAt(0)
+        if (wordList.isEmpty()) {
+            resetList()
         } else {
             //Select and remove a _word from the list
             _word.value = wordList.removeAt(0)
@@ -108,4 +145,20 @@ class GTWGameViewModel : ViewModel() {
     fun onGameFinishComplete() {
         _eventGameFinish.value = false
     }
+
+
+
+    companion object {
+
+        // Time when the game is over
+        private const val DONE = 0L
+
+        // Countdown time interval
+        private const val ONE_SECOND = 1000L
+
+        // Total time for the game
+        private const val COUNTDOWN_TIME = 60000L
+
+    }
+
 }
